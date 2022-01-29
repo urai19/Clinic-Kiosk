c8a09bce02ea7ce54e5050bbadf7867ae223e3a0
garvitgupta <garvitgupta3000@gmail.com>
2022-01-29 13:08:05 -0500

    Time Class updated

--- src/clinic/Time.java
+++ src/clinic/Time.java
@@ -1,4 +1,15 @@
 package clinic;
 
-public class Time {
+public class Time implements Comparable<Time> {
+
+    private int hour;
+    private int minute;
+
+    public boolean isValid() { }
+
+    @Override
+    public String toString() { }
+
+    @Override
+    public int compareTo(Time time) { }
 }
