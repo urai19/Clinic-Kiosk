3ca799d9755f9f0dfa6e6ff121b029de5ed06750
garvitgupta <garvitgupta3000@gmail.com>
2022-01-29 15:35:46 -0500

    Created Timeslot and Finished time

--- src/clinic/Time.java
+++ src/clinic/Time.java
@@ -5,11 +5,29 @@ public class Time implements Comparable<Time> {
     private int hour;
     private int minute;
 
-    public boolean isValid() { }
+    public boolean isValid() {
+        if (hour < 9 || hour >= 17 || minute < 0 || minute > 60) {
+            return false;
+        }
+
+        if (minute != 00 || minute != 15 || minute != 30 || minute != 45) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 
     @Override
-    public String toString() { }
+    public String toString() {
+        String output = hour + ":" + minute;
+        return output;
+    }
 
     @Override
-    public int compareTo(Time time) { }
+    public int compareTo(Time time) {
+        if (this.hour == time.hour && this.minute == time.minute) {
+            return 1;
+        } else return 0;
+    }
 }
---
+++ src/clinic/Timeslot.java
@@ -0,0 +1,11 @@
+package clinic;
+
+public class Timeslot {
+
+    private Date date;
+    private Time time;
+    @Override
+    public String toString() { }
+    @Override
+    public int compareTo(Timeslot slot) { }
+}
